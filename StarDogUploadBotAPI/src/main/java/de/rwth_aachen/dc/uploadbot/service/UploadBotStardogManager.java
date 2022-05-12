5dfb7b87bd5ea52129f751fdb7f44a5bb568c001
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2022-05-12 17:03:49 +0200

    The Stardog upload tested

--- StarDogUploadBotAPI/src/main/java/de/rwth_aachen/dc/uploadbot/service/UploadBotStardogManager.java
+++ StarDogUploadBotAPI/src/main/java/de/rwth_aachen/dc/uploadbot/service/UploadBotStardogManager.java
@@ -1,10 +1,11 @@
 package de.rwth_aachen.dc.uploadbot.service;
 
+import java.io.FileInputStream;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.riot.RDFDataMgr;
 
 public class UploadBotStardogManager {
 	B4RStardogConnection stardog= new B4RStardogConnection();
@@ -15,9 +16,8 @@ public class UploadBotStardogManager {
 	public void uploadTTL(Path path) {
 		Runnable runnable = () -> {
 			try {
-
 				Model m = ModelFactory.createDefaultModel();
-				RDFDataMgr.read(m, path.toFile().getAbsolutePath());
+				m.read(new FileInputStream(path.toFile().getAbsoluteFile()),null,"TTL");
 				
 				stardog.sendModel(m);
 			} catch (Exception e) {
@@ -28,4 +28,8 @@ public class UploadBotStardogManager {
 		thread.start();
 	}
 
+	public static void main(String[] args) {
+		UploadBotStardogManager u=new UploadBotStardogManager();
+		u.uploadTTL(Paths.get("C:\\jo\\2022-05-12_B4R-Stardog\\Connection_test1.ttl"));
+	}
 }
